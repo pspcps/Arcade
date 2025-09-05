3a6fc31251e4318fc71beb8c4cb0615b75c3f74a
prakash-seervi1 <prakash.seervi9460@gmail.com>
2025-09-05 13:46:20 +0000

    dada

--- GSP1042.sh
+++ GSP1042.sh
@@ -21,3 +21,10 @@ SELECT * FROM \`bigquery-public-data.geo_us_boundaries.zip_codes\` WHERE state_c
 EOF
 echo " • View B created."
 
+echo "Authorizing views to access the source dataset..."
+
+bq update --source --view "${PARTNER_PROJECT_ID}.${DATASET}.${VIEW_A}" "${PARTNER_PROJECT_ID}:${DATASET}"
+echo " • Authorized View A."
+
+bq update --source --view "${PARTNER_PROJECT_ID}.${DATASET}.${VIEW_B}" "${PARTNER_PROJECT_ID}:${DATASET}"
+echo " • Authorized View B."
