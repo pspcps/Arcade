58b60754abafe02ad2c4b585a4e7dd2e43c92034
prakash-seervi1 <prakash.seervi9460@gmail.com>
2025-09-08 12:21:06 +0000

    dasdas

--- GSP074.sh
+++ GSP074.sh
@@ -3,20 +3,20 @@ gcloud auth list
 gcloud config list project
 export PROJECT_ID=$(gcloud config get-value project)
 
-gsutil mb gs://$PROJECT_ID-techcps
+gsutil mb gs://$PROJECT_ID-mazekro
 
 curl https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Ada_Lovelace_portrait.jpg/800px-Ada_Lovelace_portrait.jpg --output ada.jpg
 
-gsutil cp ada.jpg gs://$PROJECT_ID-techcps
+gsutil cp ada.jpg gs://$PROJECT_ID-mazekro
 
 rm ada.jpg
 
-gsutil cp -r gs://$PROJECT_ID-techcps/ada.jpg .
+gsutil cp -r gs://$PROJECT_ID-mazekro/ada.jpg .
 
-gsutil cp gs://$PROJECT_ID-techcps/ada.jpg gs://$PROJECT_ID-techcps/image-folder/
+gsutil cp gs://$PROJECT_ID-mazekro/ada.jpg gs://$PROJECT_ID-mazekro/image-folder/
 
-gsutil ls gs://$PROJECT_ID-techcps
+gsutil ls gs://$PROJECT_ID-mazekro
 
-gsutil ls -l gs://$PROJECT_ID-techcps/ada.jpg
+gsutil ls -l gs://$PROJECT_ID-mazekro/ada.jpg
 
-gsutil acl ch -u AllUsers:R gs://$PROJECT_ID-techcps/ada.jpg
+gsutil acl ch -u AllUsers:R gs://$PROJECT_ID-mazekro/ada.jpg
