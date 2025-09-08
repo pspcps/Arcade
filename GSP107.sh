aff689968478066be30b9bc6b246ea989451b880
prakash-seervi1 <prakash.seervi9460@gmail.com>
2025-09-08 15:18:34 +0000

    dasds

---
+++ GSP107.sh
@@ -0,0 +1,77 @@
+
+gcloud auth list
+
+export PROJECT_ID=$DEVSHELL_PROJECT_ID
+
+cat > inspect-request.json <<EOF_CP
+{
+  "item":{
+    "value":"My phone number is (206) 555-0123."
+  },
+  "inspectConfig":{
+    "infoTypes":[
+      {
+        "name":"PHONE_NUMBER"
+      },
+      {
+        "name":"US_TOLLFREE_PHONE_NUMBER"
+      }
+    ],
+    "minLikelihood":"POSSIBLE",
+    "limits":{
+      "maxFindingsPerItem":0
+    },
+    "includeQuote":true
+  }
+}
+EOF_CP
+
+ACCESS_TOKEN=$(gcloud auth application-default print-access-token)
+
+curl -s \
+  -H "Authorization: Bearer $ACCESS_TOKEN" \
+  -H "Content-Type: application/json" \
+  https://dlp.googleapis.com/v2/projects/$PROJECT_ID/content:inspect \
+  -d @inspect-request.json -o inspect-output.txt
+
+
+cat inspect-output.txt
+
+gsutil cp inspect-output.txt gs://$PROJECT_ID-bucket
+
+
+
+cat > new-inspect-file.json <<EOF_CP
+{
+  "item": {
+     "value":"My email is test@gmail.com",
+   },
+   "deidentifyConfig": {
+     "infoTypeTransformations":{
+          "transformations": [
+            {
+              "primitiveTransformation": {
+                "replaceWithInfoTypeConfig": {}
+              }
+            }
+          ]
+        }
+    },
+    "inspectConfig": {
+      "infoTypes": {
+        "name": "EMAIL_ADDRESS"
+      }
+    }
+}
+EOF_CP
+
+
+curl -s \
+  -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+  -H "Content-Type: application/json" \
+  https://dlp.googleapis.com/v2/projects/$PROJECT_ID/content:deidentify \
+  -d @new-inspect-file.json -o redact-output.txt
+
+cat redact-output.txt
+
+gsutil cp redact-output.txt gs://$PROJECT_ID-bucket
