56109a6fea839d1b529fe63905ee0b7ea2fca32d
prakash-seervi1 <prakash.seervi9460@gmail.com>
2025-09-09 19:10:12 +0000

    dasda

--- ARC113-form3.sh
+++
@@ -1,9 +0,0 @@
-gcloud pubsub topics create gcloud-pubsub-topic
-gcloud pubsub subscriptions create pubsub-subscription-message --topic=gcloud-pubsub-topic
-gcloud pubsub topics publish gcloud-pubsub-topic --message="Hello World"
-
-sleep 10
-
-gcloud pubsub subscriptions pull pubsub-subscription-message --limit 5
-
-gcloud pubsub snapshots create pubsub-snapshot --subscription=pubsub-subscription-message
