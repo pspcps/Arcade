68a97862a44e9ed3983e6b609403244ceb75c320
prakash-seervi1 <prakash.seervi9460@gmail.com>
2025-09-10 15:52:07 +0000

    dada

---
+++ ARC118.sh
@@ -0,0 +1,30 @@
+
+
+gcloud auth list
+
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+gcloud services enable run.googleapis.com
+gcloud services enable eventarc.googleapis.com
+
+gcloud pubsub topics create $DEVSHELL_PROJECT_ID-topic
+
+gcloud  pubsub subscriptions create --topic $DEVSHELL_PROJECT_ID-topic $DEVSHELL_PROJECT_ID-topic-sub
+
+gcloud run deploy pubsub-events \
+  --image=gcr.io/cloudrun/hello \
+  --platform=managed \
+  --region=$REGION \
+  --allow-unauthenticated
+
+gcloud eventarc triggers create pubsub-events-trigger \
+  --location=$REGION \
+  --destination-run-service=pubsub-events \
+  --destination-run-region=$REGION \
+  --transport-topic=$DEVSHELL_PROJECT_ID-topic \
+  --event-filters="type=google.cloud.pubsub.topic.v1.messagePublished"
+
+gcloud pubsub topics publish $DEVSHELL_PROJECT_ID-topic \
+  --message="Test message"
