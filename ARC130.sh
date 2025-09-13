53b3f393b7b6724ac12b5fa12d5c2f1d5cb67893
prakash-seervi1 <prakash.seervi9460@gmail.com>
2025-09-13 17:33:29 +0000

    dada

--- ARC130.sh
+++ ARC130.sh
@@ -4,13 +4,21 @@ echo "Detected VM zone: $ZONE"
 
 # Fetch current project ID
 export GOOGLE_CLOUD_PROJECT=$(gcloud config get-value core/project)
+# Create the API key and capture full JSON output
 
-# Create an API key (or use an existing one)
-export API_KEY=$(gcloud services api-keys create --display-name="natural-language-api-key" --project=$GOOGLE_CLOUD_PROJECT --format="value(keyString)")
+API_KEY_JSON=$(gcloud services api-keys create \
+  --display-name="API key" \
+  --project=$GOOGLE_CLOUD_PROJECT \
+  --format=json)
+
+API_KEY=$(echo "$API_KEY_JSON" | jq -r '.keyString // .response.keyString')
+
+
+# Print confirmation
 echo "Generated API Key: $API_KEY"
 
 # SSH into the VM and execute the remote script
 gcloud compute ssh lab-vm --zone=$ZONE --quiet --command \
-"curl -L -o  https://raw.githubusercontent.com/pspcps/Arcade/refs/heads/main/ARC130-1.sh && \
+"curl -LO https://raw.githubusercontent.com/pspcps/Arcade/refs/heads/main/ARC130-1.sh && \
 chmod +x ARC130-1.sh && \
 API_KEY=$API_KEY ./ARC130-1.sh"
