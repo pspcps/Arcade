30a11a93d2b60deac848dc3aa7d1ef6c64fc7972
prakash-seervi1 <prakash.seervi9460@gmail.com>
2025-09-22 12:25:43 +0000

    dafd

---
+++ GSP449.sh
@@ -0,0 +1,32 @@
+
+
+
+gcloud auth list
+
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+export PROJECT_ID=$(gcloud config get-value project)
+
+gcloud config set compute/zone "$ZONE"
+
+gcloud config set compute/region "$REGION"
+
+gsutil cp gs://spls/gsp449/gke-cloud-sql-postgres-demo.tar.gz .
+tar -xzvf gke-cloud-sql-postgres-demo.tar.gz
+
+cd gke-cloud-sql-postgres-demo
+
+PG_EMAIL=$(gcloud config get-value account)
+
+./create.sh dbadmin $PG_EMAIL
+
+
+sleep 10
+
+POD_ID=$(kubectl --namespace default get pods -o name | cut -d '/' -f 2)
+
+kubectl expose pod $POD_ID --port=80 --type=LoadBalancer
+
+kubectl get svc
