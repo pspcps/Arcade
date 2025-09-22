84b9614c10a6672515ade06d0f74234cd3cc6092
prakash-seervi1 <prakash.seervi9460@gmail.com>
2025-09-22 13:30:14 +0000

    dasd

---
+++ GSP1177.sh
@@ -0,0 +1,67 @@
+
+
+
+gcloud auth list
+
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+git clone https://github.com/Redislabs-Solution-Architects/gcp-microservices-demo-qwiklabs.git
+pushd gcp-microservices-demo-qwiklabs
+
+cat <<EOF > terraform.tfvars
+gcp_project_id = "$(gcloud config list project \
+ --format='value(core.project)')"
+gcp_region = "$REGION"
+EOF
+
+terraform init
+
+terraform apply -auto-approve
+
+export REDIS_DEST=`terraform output db_private_endpoint | tr -d '"'`
+export REDIS_DEST_PASS=`terraform output db_password | tr -d '"'`
+export REDIS_ENDPOINT="${REDIS_DEST},user=default,password=${REDIS_DEST_PASS}"
+
+gcloud container clusters get-credentials \
+$(terraform output -raw gke_cluster_name) \
+--region $(terraform output -raw region)
+
+kubectl get service frontend-external -n redis
+
+kubectl config set-context --current --namespace=redis
+
+kubectl get deployment cartservice -o jsonpath='{.spec.template.spec.containers[0].env}' | jq
+
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: redis-creds
+type: Opaque
+stringData:
+  REDIS_SOURCE: redis://redis-cart:6379
+  REDIS_DEST: redis://${REDIS_DEST}
+  REDIS_DEST_PASS: ${REDIS_DEST_PASS}
+EOF
+
+kubectl apply -f https://raw.githubusercontent.com/Redislabs-Solution-Architects/gcp-microservices-demo-qwiklabs/main/util/redis-migrator-job.yaml
+
+kubectl get deployment cartservice -o jsonpath='{.spec.template.spec.containers[0].env}' | jq
+
+kubectl patch deployment cartservice --patch '{"spec":{"template":{"spec":{"containers":[{"name":"server","env":[{"name":"REDIS_ADDR","value":"'$REDIS_ENDPOINT'"}]}]}}}}'
+
+kubectl get deployment cartservice -o jsonpath='{.spec.template.spec.containers[0].env}' | jq
+
+kubectl patch deployment cartservice --patch '{"spec":{"template":{"spec":{"containers":[{"name":"server","env":[{"name":"REDIS_ADDR","value":"redis-cart:6379"}]}]}}}}'
+
+kubectl get deployment cartservice -o jsonpath='{.spec.template.spec.containers[0].env}' | jq
+
+kubectl patch deployment cartservice --patch '{"spec":{"template":{"spec":{"containers":[{"name":"server","env":[{"name":"REDIS_ADDR","value":"'$REDIS_ENDPOINT'"}]}]}}}}'
+
+kubectl get deployment cartservice -o jsonpath='{.spec.template.spec.containers[0].env}' | jq
+
+
+kubectl delete deploy redis-cart
+
