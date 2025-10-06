4606d7a486d9aa89a72c86e00cf3f1cbfc992b28
prakash-seervi1 <prakash.seervi9460@gmail.com>
2025-10-06 17:25:33 +0000

    dasd

--- GSP100.sh
+++ GSP100.sh
@@ -19,5 +19,16 @@ kubectl create deployment hello-server --image=gcr.io/google-samples/hello-app:1
 kubectl expose deployment hello-server --type=LoadBalancer --port 8080
 
 
+#!/bin/bash
+
+# Define color variables
+YELLOW_TEXT=$'\033[0;93m'
+BOLD=`tput bold`
+RESET=`tput sgr0`
+
+
+# Gather inputs for the required variables, cycling through colors
+echo -n -e "${BOLD}${YELLOW_TEXT}⚠️ ☢️ Please Check Progress Till Task 4, and than Press Enter Than Y For Deleting Instance ${RESET} " 
+read KEY 
 
 gcloud container clusters delete lab-cluster
